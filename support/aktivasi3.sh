de0a7625597247b11963f93eab093ee3409d9b46
Ori Grata <42106367+origrata@users.noreply.github.com>
2024-04-15 13:14:04 +0700

    Perbaikan post json ke api cloudflare
    Perbaikan struktur post curl to cloudflare

--- support/aktivasi3.sh
+++ support/aktivasi3.sh
@@ -1,5 +1,5 @@
-curl -s -X POST "https://api.cloudflare.com/client/v4/zones/tulisidzonedisini/dns_records" \
-     -H "X-Auth-Email: nama@email.com" \
-     -H "X-Auth-Key: tulisauthkeydisini" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"unik.xcodehoster.com","content":"ip","ttl":120,"priority":10,"proxied":true}' > /dev/null
+curl -X POST https://api.cloudflare.com/client/v4/zones/tulisidzonedisini/dns_records \
+     -H 'X-Auth-Email: nama@email.com' \
+     -H 'X-Auth-Key: tulisauthkeydisini' \
+     -H 'Content-Type: application/json' \
+     -d '{"type":"A","name":"unik.xcodehoster.com","content":"ip","ttl":120,"priority":10,"proxied":true}' > /dev/null
